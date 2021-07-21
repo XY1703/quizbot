77838b545a53d0980854c5fa3ef29470bfea494d
LuckyDraw777 <2356448Ufkz>
2021-07-21 15:19:13 +0300

    Update Receiver was created

--- src/main/java/com/luckydraw/quizbot/bot/QuizBot.java
+++ src/main/java/com/luckydraw/quizbot/bot/QuizBot.java
@@ -3,16 +3,22 @@ package com.luckydraw.quizbot.bot;
 import com.luckydraw.quizbot.model.User;
 import com.luckydraw.quizbot.service.ThemeService;
 import com.luckydraw.quizbot.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.Serializable;
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class QuizBot extends TelegramLongPollingBot {
 
     @Autowired
@@ -27,6 +33,11 @@ public class QuizBot extends TelegramLongPollingBot {
     @Value("${telegrambot.token}")
     private String token;
 
+    private final UpdateReceiver updateReceiver;
+
+    public QuizBot(UpdateReceiver updateReceiver) {
+        this.updateReceiver = updateReceiver;
+    }
 
     @Override
     public String getBotUsername() {
@@ -40,18 +51,22 @@ public class QuizBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        /*SendMessage sm = new SendMessage();
-        sm.setChatId(String.valueOf(update.getMessage().getChatId()));
-        sm.setText(userService.getInstance(update.getMessage().getText()).toString());
+        List<PartialBotApiMethod<? extends Serializable>> messagesToSend = updateReceiver.handle(update);
+
+        if (messagesToSend != null && !messagesToSend.isEmpty()) {
+            messagesToSend.forEach(response -> {
+                if (response instanceof SendMessage) {
+                    executeWithExceptionCheck((SendMessage) response);
+                }
+            });
+        }
+    }
+
+    public void executeWithExceptionCheck(SendMessage sendMessage) {
         try {
-            execute(sm);
+            execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
-        }*/
-
-        User user = new User(Math.toIntExact(update.getMessage().getChatId()));
-        userService.registration(user);
-
-
+        }
     }
 }
--- src/main/java/com/luckydraw/quizbot/bot/UpdateReceiver.java
+++ src/main/java/com/luckydraw/quizbot/bot/UpdateReceiver.java
@@ -2,16 +2,68 @@ package com.luckydraw.quizbot.bot;
 
 
 import com.luckydraw.quizbot.bot.handler.Handler;
+import com.luckydraw.quizbot.model.User;
+import com.luckydraw.quizbot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.io.Serializable;
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class UpdateReceiver {
 
-   /* private final List<Handler> handlers;
+    private final List<Handler> handlers;
 
-    public UpdateReceiver(List<Handler> handlers) {
+    private UserService userService;
+
+    public UpdateReceiver(List<Handler> handlers, @Autowired UserService userService) {
         this.handlers = handlers;
+        this.userService = userService;
+    }
+
+
+    public List<PartialBotApiMethod<? extends Serializable>> handle(Update update) {
+        try {
+            if (isMessageWithText(update)) {
+                Message message = update.getMessage();
+
+                User user = userService.getInstance(String.valueOf(message.getChatId()));
+
+                if (user == null) userService.registration(new User(Math.toIntExact(message.getChatId())));
+
+                return getHandlerByState(user.getState()).handle(user, message.getText());
+            }
+            throw new UnsupportedOperationException();
+
+        } catch (UnsupportedOperationException e) {
+            return Collections.emptyList();
+        }
+    }
+
+
+    private Handler getHandlerByState(State state) {
+        return handlers.stream()
+                .filter(h -> h.operatedBotState() != null)
+                .filter(h -> h.operatedBotState().equals(state))
+                .findAny()
+                .orElseThrow(UnsupportedOperationException::new);
+    }
+
+    /*private Handler getHandlerByCallBackQuery(String query) {
+        return handlers.stream()
+                .filter(h -> h.operatedCallBackQuery().stream()
+                        .anyMatch(query::startsWith))
+                .findAny()
+                .orElseThrow(UnsupportedOperationException::new);
     }*/
+
+
+    private boolean isMessageWithText(Update update) {
+        return !update.hasCallbackQuery() && update.hasMessage() && update.getMessage().hasText();
+    }
 }
